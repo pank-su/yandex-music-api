d0b685d5374a67fadd1c9400c85d4df2cfee19a9
Вася Панков <frostkslo1@yandex.ru>
2024-01-05 12:39:41 +0300

    update tests

--- library/src/commonTest/kotlin/ChartTest.kt
+++ library/src/commonTest/kotlin/ChartTest.kt
@@ -1,4 +1,5 @@
 import dsl.client
+import io.getenv
 import kotlinx.coroutines.test.runTest
 import model.landing.ChartOption
 import kotlin.test.Test
@@ -6,6 +7,7 @@ import kotlin.test.Test
 class ChartTest {
     @Test
     fun gettingTest() = runTest {
+        getenv("token") ?: return@runTest
         val chart = client { }.chart(ChartOption.World)
         println(chart)
     }
--- library/src/commonTest/kotlin/GenreTest.kt
+++ library/src/commonTest/kotlin/GenreTest.kt
@@ -1,10 +1,12 @@
 import dsl.client
+import io.getenv
 import kotlinx.coroutines.test.runTest
 import kotlin.test.Test
 
 class GenreTest {
     @Test
     fun gettingTest() = runTest{
+        getenv("token") ?: return@runTest
         client {  }.genres()
     }
 }
