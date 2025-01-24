1f3adecd736ae0f29262d9dbe82fa3839c8bfe95
pank-su <frostkslo1@yandex.ru>
2025-01-24 19:34:57 +0300

    :truck: сборка моделей связанных с плейлистом в один файл

--- core/src/commonMain/kotlin/su/pank/yamapi/model/playlist/MadeFor.kt
+++
@@ -1,7 +0,0 @@
-package model.playlist
-
-import kotlinx.serialization.Serializable
-import su.pank.yamapi.account.model.User
-
-@Serializable
-data class MadeFor(val userInfo: User, val caseForms: CaseForms)
--- core/src/commonMain/kotlin/su/pank/yamapi/model/playlist/Tag.kt
+++
@@ -1,6 +0,0 @@
-package model.playlist
-
-import kotlinx.serialization.Serializable
-
-@Serializable
-data class Tag(val id: String, val value: String, val name: String, val ogDescription: String)
